9a8632a0d638593ec31caf456e058c1b3a4513ce
zuhalal <zuhalalimulhadi19@gmail.com>
2024-07-27 15:38:14 +0700

    fix: add cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+from app.router import router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
